25ac627ef82298c46c539a16b90045640eec9501
gabydelaf <90404901+gabydelaf@users.noreply.github.com>
2023-05-25 11:19:46 +0200

    Update demux_tb.vhdl

--- demux_tb.vhdl
+++ demux_tb.vhdl
@@ -1,44 +1,80 @@
-Library IEEE;
-use IEEE.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ 
+ENTITY demux_tb IS
+END demux_tb;
+ 
+ARCHITECTURE behavior OF demux_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT sseg_demux
+    PORT(
+         sseg_in : IN  std_logic_vector(6 downto 0);
+         nsel0 : IN  std_logic;
+         nsel1 : IN  std_logic;
+         nsel2 : IN  std_logic;
+         einer_out : OUT  std_logic_vector(6 downto 0);
+         zehner_out : OUT  std_logic_vector(6 downto 0);
+         hundert_out : OUT  std_logic_vector(6 downto 0)
+        );
+    END COMPONENT;
+    
 
-Entity demux_tb Is
-End demux_tb;
+   --Inputs
+   signal sseg_in : std_logic_vector(6 downto 0) := (others => '0');
+   signal nsel0 : std_logic := '0';
+   signal nsel1 : std_logic := '0';
+   signal nsel2 : std_logic := '0';
 
-Architecture behavior of demux_tb is
-  Component demux
-    Port (sseg_in : In std_logic_vector(6 downto 0);
-          nsel0, nsel1, nsel2: In std_logic;
-          einer_out : Out std_logic_vector(6 downto 0)
-          zehner_out : Out std_logic_vector(6 downto 0)
-          hundert_out : Out std_logic_vector(6 downto 0));
-  End component; 
-  
-signal sseg_in: std_logic_vector (6 downto 0) := (others => '0');
-signal nsel0, nsel1, nsel2: In std_logic;
-signal einer_out, zehner_out, hundert_out : std_logic_vector (6 down to 0);
+ 	--Outputs
+   signal einer_out : std_logic_vector(6 downto 0);
+   signal zehner_out : std_logic_vector(6 downto 0);
+   signal hundert_out : std_logic_vector(6 downto 0);
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: sseg_demux PORT MAP (
+          sseg_in => sseg_in,
+          nsel0 => nsel0,
+          nsel1 => nsel1,
+          nsel2 => nsel2,
+          einer_out => einer_out,
+          zehner_out => zehner_out,
+          hundert_out => hundert_out
+        );
 
-Begin 
--- Instantiate the Unit Under Test (UUT) --
-uut: sseg Port Map (
-      sseg_in => sseg_in,  
-      nsel0 => nsel0,
-      nsel1 => nsel1,
-      nsel2 => nsel2,
-      einer_out => einer_out,
-      zehner_out => zehner_out,
-      hundert_out => hundert_out
-      );
-      
-stim_proc: Process 
-  Begin
-  sseg_in <= "0000"; wait for 100ns; 
-  nsel0 <= "0000"; wait for 100ns; 
-  nsel1 <= "0000"; wait for 100ns; 
-  nsel2 <= "0000"; wait for 100ns; 
-  einer_out <= "0000"; wait for 100ns; 
-  zehner_out <= "0000"; wait for 100ns; 
-  hundert_out <= "0000"; wait for 100ns; 
-  wait;
- End Process;
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      sseg_in <= "1111111"; 
+		nsel0 <= '0';
+		nsel1 <= '1';  
+		nsel2 <= '1'; wait for 500ns;
+		
+		nsel0 <= '1';
+		nsel1 <= '0';  
+		nsel2 <= '0'; wait for 100ns;
+		
+		nsel0 <= '1';
+		nsel1 <= '0';  
+		nsel2 <= '1'; wait for 100ns;
+		
+		nsel0 <= '1';
+		nsel1 <= '1';  
+		nsel2 <= '0'; wait for 100ns;
+		
+		nsel0 <= '1';
+		nsel1 <= '1';  
+		nsel2 <= '1'; wait for 100ns;
+		
+		nsel0 <= '0';
+		nsel1 <= '0';  
+		nsel2 <= '0'; wait for 100ns;
 
-End;
+      wait;
+   end process;
+
+END;
