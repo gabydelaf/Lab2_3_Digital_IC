a62d49b7d9fc94d60f4a55b134d349002e3939db
gabydelaf <90404901+gabydelaf@users.noreply.github.com>
2023-05-25 11:25:06 +0200

    Update signal_mapper.vhdl

--- signal_mapper.vhdl
+++ signal_mapper.vhdl
@@ -1,17 +1,20 @@
-Library IEEE;
-use IEEE.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-Entity signal_mapper is 
-  Port (sseg_out : In std_logic(6 downto 0);
+entity signal_mapper is
+	Port (sseg_out : In std_logic_vector(6 downto 0);
         a, b, c, d, e, f, g : Out std_logic);
-End signal_mapper;
+end signal_mapper;
 
-Architecture of signal_mapper Is
-  a <= sseg_out(0);
-  b <= sseg_out(1);
-  c <= sseg_out(2);
-  d <= sseg_out(3);
-  e <= sseg_out(4);
-  f <= sseg_out(5);
-  g <= sseg_out(6);
-End Architecture signal_mapper;
+architecture Behavioral of signal_mapper is
+
+begin
+	a <= sseg_out(0);
+	b <= sseg_out(1);
+	c <= sseg_out(2);
+	d <= sseg_out(3);
+	e <= sseg_out(4);
+	f <= sseg_out(5);
+	g <= sseg_out(6);
+
+end Behavioral;
