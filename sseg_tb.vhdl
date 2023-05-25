d5c647dd910819325f1d13355070aa8e37f4df13
gabydelaf <90404901+gabydelaf@users.noreply.github.com>
2023-05-25 11:33:31 +0200

    Update sseg_tb.vhdl

--- sseg_tb.vhdl
+++ sseg_tb.vhdl
@@ -1,34 +1,54 @@
-Library IEEE;
-use IEEE.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY sseg_tb IS
+END sseg_tb;
+ 
+ARCHITECTURE behavior OF sseg_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT sseg_with1
+    PORT(
+         a : IN  std_logic_vector(3 downto 0);
+         sseg_out : OUT  std_logic_vector(6 downto 0)
+        );
+    END COMPONENT;
+    
 
-Entity sseg_tb Is
-End sseg_tb;
+   --Inputs
+   signal a : std_logic_vector(3 downto 0) := (others => '0');
 
-Architecture behavior of sseg_tb is
-  Component sseg
-    Port (a: in std_logic vector (3 downto 0);
-          sseg_out: Out std_logic_vector (6 downto 0) );
-  End component; 
-  
-signal a: std_logic_vector (3 downto 0) := (others => '0');
-signal sseg_out std_logic_vector (6 down to 0);
+ 	--Outputs
+   signal sseg_out : std_logic_vector(6 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
 
-Begin 
-uut: sseg Port Map (a => a, sseg_out => sseg_out); --Device Under Verification
-stim_proc: Process 
-  Begin
-  a <= "0000"; wait for 10ns; 
-  a <= "0001"; wait for 10ns;
-  a <= "0010"; wait for 10ns;
-  a <= "0011"; wait for 10ns;
-  a <= "0100"; wait for 10ns; 
-  a <= "0101"; wait for 10ns;
-  a <= "0110"; wait for 10ns;
-  a <= "0111"; wait for 10ns;
-  a <= "1000"; wait for 10ns; 
-  a <= "1001"; wait for 10ns;
-  a <= "1010"; wait for 10ns;  
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: sseg_with1 PORT MAP (
+          a => a,
+          sseg_out => sseg_out
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+    a <= "0000"; wait for 10ns; 
+		a <= "0001"; wait for 10ns;
+		a <= "0010"; wait for 10ns;
+		a <= "0011"; wait for 10ns;
+		a <= "0100"; wait for 10ns; 
+		a <= "0101"; wait for 10ns;
+		a <= "0110"; wait for 10ns;
+		a <= "0111"; wait for 10ns;
+		a <= "1000"; wait for 10ns; 
+		a <= "1001"; wait for 10ns;
+		a <= "1010"; wait for 10ns;
   wait;
- End Process;
+   end process;
 
-End;
+END;
