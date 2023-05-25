6548bb3284fe7aa31273198c4cf6b6862ceb94ee
gabydelaf <90404901+gabydelaf@users.noreply.github.com>
2023-05-25 10:33:58 +0200

    Update sseg_decoder.vhdl

--- sseg_decoder.vhdl
+++ sseg_decoder.vhdl
@@ -1,25 +1,27 @@
-Library IEEE;
-use IEEE.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-Entity sseg_decoder is 
-  Port (in0 : In std_logic;
+entity sseg_decoder is
+	Port (in0 : In std_logic;
         in1: In std_logic;
         nsel_e, nsel_z, nsel_h : Out std_logic);
-End sseg_decoder;
+end sseg_decoder;
 
-Architecture of sseg_decoder Is
-Begin
-  decoder: Process(in0, in1) is
-  Begin
+architecture Behavioral of sseg_decoder is
+
+begin
+	decoder: Process(in0, in1) is
+	Begin
     nsel_e <= '1';
     nsel_z <= '1';
     nsel_h <= '1';
     
     If     in0 = '0' And in1 = '0' then nsel_e <= '0';
-    Elseif in0 = '1' And in1 = '0' then nsel_e <= '0';
-    Elseif in0 = '0' And in1 = '1' then nsel_e <= '0';
+    Elsif in0 = '1' And in1 = '0' then nsel_z <= '0';
+    Elsif in0 = '0' And in1 = '1' then nsel_h <= '0';
     Else Null;
     End If;
   end Process decoder;
-  
-End Architecture sseg_decoder;
+
+end Behavioral;
+
