cd5bf2b1d80e713744939c0dbaa7e11c6b2ab066
gabydelaf <90404901+gabydelaf@users.noreply.github.com>
2023-05-25 11:04:48 +0200

    Update decoder_tb.vhdl

--- decoder_tb.vhdl
+++ decoder_tb.vhdl
@@ -1,26 +1,62 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-entity sseg_decoder is
-	Port (in0 : In std_logic;
-        in1: In std_logic;
-        nsel_e, nsel_z, nsel_h : Out std_logic);
-end sseg_decoder;
-
-architecture Behavioral of sseg_decoder is
-
-begin
-	decoder: Process(in0, in1) is
-	Begin
-    nsel_e <= '1';
-    nsel_z <= '1';
-    nsel_h <= '1';
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY decoder_tb IS
+END decoder_tb;
+ 
+ARCHITECTURE behavior OF decoder_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT sseg_decoder
+    PORT(
+         in0 : IN  std_logic;
+         in1 : IN  std_logic;
+         nsel_e : OUT  std_logic;
+         nsel_z : OUT  std_logic;
+         nsel_h : OUT  std_logic
+        );
+    END COMPONENT;
     
-    If     in0 = '0' And in1 = '0' then nsel_e <= '0';
-    Elsif in0 = '1' And in1 = '0' then nsel_z <= '0';
-    Elsif in0 = '0' And in1 = '1' then nsel_h <= '0';
-    Else Null;
-    End If;
-  end Process decoder;
 
-end Behavioral;
+   --Inputs
+   signal in0 : std_logic := '0';
+   signal in1 : std_logic := '0';
+
+ 	--Outputs
+   signal nsel_e : std_logic;
+   signal nsel_z : std_logic;
+   signal nsel_h : std_logic;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: sseg_decoder PORT MAP (
+          in0 => in0,
+          in1 => in1,
+          nsel_e => nsel_e,
+          nsel_z => nsel_z,
+          nsel_h => nsel_h
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      in0 <= '0'; 
+		in1 <= '0'; wait for 100ns; 
+		
+		in0 <= '1'; 
+		in1 <= '0'; wait for 100ns; 
+		
+		in0 <= '0'; 
+		in1 <= '1'; wait for 100ns; 
+		
+		in0 <= '1'; 
+		in1 <= '1'; wait for 100ns; 
+
+      wait;
+   end process;
+
+END;
+
