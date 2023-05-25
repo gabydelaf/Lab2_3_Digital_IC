8441501975ebdb1b370350799c3b1db9d4674719
gabydelaf <90404901+gabydelaf@users.noreply.github.com>
2023-05-25 11:04:03 +0200

    Update sseg_mux.vhdl

--- sseg_mux.vhdl
+++ sseg_mux.vhdl
@@ -1,22 +1,23 @@
-Library IEEE;
-use IEEE.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-Entity sseg_mux is 
-  Port (in0, in1, in2 : In std_logic_vector(3 downto 0);
+entity sseg_mux is
+	Port (in0, in1, in2 : In std_logic_vector(3 downto 0);
         nsel0, nsel1, nsel2: In std_logic;
         out1 : Out std_logic_vector(3 downto 0));
-End sseg_mux;
+end sseg_mux;
 
-Architecture of sseg_mux Is
-Begin
-  mux: Process(in0, in1, in2, nsel0, nsel1, nsel2) is
-  Begin
+architecture Behavioral of sseg_mux is
+
+begin
+	mux: Process(in0, in1, in2, nsel0, nsel1, nsel2) is
+	Begin
   
-    If     nsel0 = '0' And nsel1 = '1' And nsel2 = '1' then out1 <= in0;
+    If    nsel0 = '0' And nsel1 = '1' And nsel2 = '1' then out1 <= in0;
     Elsif nsel0 = '1' And nsel1 = '0' And nsel2 = '1' then out1 <= in1;
     Elsif nsel0 = '1' And nsel1 = '1' And nsel2 = '0' then out1 <= in2;
     Else Null;
     End If;
   end Process mux;
-  
-End Architecture sseg_mux;
+
+end Behavioral;
