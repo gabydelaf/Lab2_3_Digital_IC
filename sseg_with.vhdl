ec1571a4a23391693f2152ab9b65950e66e01969
gabydelaf <90404901+gabydelaf@users.noreply.github.com>
2023-05-25 08:49:57 +0200

    Update sseg_with.vhdl

--- sseg_with.vhdl
+++ sseg_with.vhdl
@@ -1,32 +1,26 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-
-Entity sseg Is
+entity sseg_with1 is
 Port ( a : In std_logic_vector (3 downto 0); 
       sseg_out : out Std_logic_vector (6 downto 0)); 
-      End sseg;
-      
--- For cathod configuration
-Architecture sseg_case of sseg Is
-Begin 
-Process (a)
+end sseg_with1;
+
+architecture Behavioral of sseg_with1 is
+ 
 begin
-  case a is 
-  
-	when "0000" => sseg_out <= "0111111"; --0
-	when "0001" => sseg_out <= "0000110"; --1        
-	when "0010" => sseg_out <= "1011011"; --2
-	when "0011" => sseg_out <= "1001111"; --3
-	when "0100" => sseg_out <= "1100110"; --4
-	when "0101" => sseg_out <= "1101101"; --5
-	when "0110" => sseg_out <= "1111101"; --6
-	when "0111" => sseg_out <= "0000111"; --7
-	when "1000" => sseg_out <= "1111111"; --8
-	when "1001" => sseg_out <= "1001111"; --9
+	with a select 
+		sseg_out <= "0111111" when "0000", --0
+					   "0000110" when "0001", --1        
+					   "1011011" when "0010", --2
+						"1001111" when "0011", --3
+						"1100110" when "0100", --4
+						"1101101" when "0101", --5
+						"1111101" when "0110", --6
+						"0000111" when "0111", --7
+						"1111111" when "1000", --8
+						"1001111" when "1001", --9
 
-	when others => sseg_out <= "0000000";
-end case;
-end process;
+						"0000000" when others;
 
-End Architecture sseg_case;
+end Behavioral;
